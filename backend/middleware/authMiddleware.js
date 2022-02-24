f81bd62b222a86e2ea4cf980940e3869fa2a4151
cansoylu <hsyncn17@gmail.com>
2022-02-24 21:55:57 +0300

    added frontend

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,37 @@
+const jwt = require("jsonwebtoken");
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+
+const protect = asyncHandler(async (req, res, next) => {
+  let token;
+
+  console.log(token, "1");
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+
+      console.log(token, "2");
+
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      req.user = await User.findById(decoded.id).select("-password");
+
+      next();
+    } catch (error) {
+      console.log(error);
+      res.status(401);
+      throw new Error("Not authorized");
+    }
+  }
+
+  if (!token) {
+    console.log("no token");
+    throw new Error("Not authorized , no token");
+  }
+});
+
+module.exports = { protect };
